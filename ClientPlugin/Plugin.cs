c3d67c5cddd067a05615d627d587552dd1d8b15c
Viktor Ferenczi <viktor@ferenczi.eu>
2026-05-17 03:26:57 +0200

    1.0.6

--- ClientPlugin/Plugin.cs
+++ ClientPlugin/Plugin.cs
@@ -8,8 +8,8 @@ using VRage.Plugins;
 #if !DEV_BUILD
 using System.Reflection;
 
-[assembly: AssemblyVersion("1.0.5.0")]
-[assembly: AssemblyFileVersion("1.0.5.0")]
+[assembly: AssemblyVersion("1.0.6.0")]
+[assembly: AssemblyFileVersion("1.0.6.0")]
 #endif
 
 namespace ClientPlugin;
